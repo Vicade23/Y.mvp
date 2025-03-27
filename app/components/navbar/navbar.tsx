4b3aab829c21988a05c22ea9a2876718d1dfe0df
Emmanuel Victor <emavec2002@gmail.com>
2025-03-27 09:33:13 -0700

    fixed layout

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -14,7 +14,17 @@ const Navbar = () => {
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [scrollY, setScrollY] = useState<number>(0)
 
+    const scrollPosition = () => {
+
+    }
+
+    useEffect(() => {
+        const handleScroll = () => setScrollY(window.scrollY);
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [])
     
 //   useEffect(() => {
 //     if (typeof window !== "undefined") {
@@ -23,93 +33,96 @@ const Navbar = () => {
 //   }, []);
     
   return (
-    <div className='navbar border-bottom'>
-        <div className="container px-3">
-            <div className="d-flex justify-content-between align-items-center w-100">
-                <div className="d-flex justify-content-between align-items-center">
+    <div className="nav-container">
 
-                    <div className="logo-container"><Link href='/'>{BRANDNAME}</Link></div>
+        <div className={` navbar border-bottom ${scrollY >= 50 && 'sticky-mode'}`}>
+            <div className="container px-3">
+                <div className="d-flex justify-content-between align-items-center w-100">
+                    <div className="d-flex justify-content-between align-items-center">
 
-                </div>
-                <div className="navigators-container d-none d-lg-flex justify-content-center align-items-center">
-                    <ul className='d-flex justify-content-center align-items-center m-0 p-0'>
-                        <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
-                            <Link href={'/'}>Home</Link>
-                        </li>
-                        <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
-                            <Link href={'/'}>Pricing</Link>
-                        </li>
-                        <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
-                            <Link href={'/'}>About us</Link>
-                        </li>
-                        <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
-                            <Link href={'/'}>Contact</Link>
-                        </li>
-                    </ul>
-                </div>
+                        <div className="logo-container"><Link href='/'>{BRANDNAME}</Link></div>
 
-                <div className="auth-btns-container d-flex">
-                    <div className="login-btn-container pe-4 pe-lg-4 d-none d-sm-block">
-                        <Link href={'/login'}>
-                            <button className="btn">Log in</button>
-                        </Link>
                     </div>
-                    <div className="signup-btn-container d-none d-sm-block">
-                        <Link href={'/signup'}>
-                            <button className="btn">Sign up</button>
-                        </Link>
-                    </div>
-                    
-                    <div className="signup-btn-container d-block d-sm-none">
-                        <Link href={'/login'}>
-                            <button className="btn">Log in</button>
-                        </Link>
-                    </div>
-
-                    <div className="launch-offcanvas-btn-container ms-3 ms-sm-4 ms-lg-4 d-flex d-lg-none justify-content-between align-items-center d-lg-none">
-                        <button className="btn d-flex justify-content-between align-items-center"  onClick={handleShow} >
-                            <RxHamburgerMenu />
-                        </button>
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
-
-        <div className="d-none d-lg-block navbar-offcanvas">
-
-            <Offcanvas show={show} onHide={handleClose}>
-                <Offcanvas.Header className='border-bottom' closeButton>
-                <Offcanvas.Title>{BRANDNAME}</Offcanvas.Title>
-                </Offcanvas.Header>
-                <Offcanvas.Body>
-                    <div className="offcanvas-container w-100">
-                        <ul className="ul p-0 m-0">
-                            <li className='border-bottom nav-lists'>
+                    <div className="navigators-container d-none d-lg-flex justify-content-center align-items-center">
+                        <ul className='d-flex justify-content-center align-items-center m-0 p-0'>
+                            <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
                                 <Link href={'/'}>Home</Link>
                             </li>
-                            <li className='border-bottom nav-lists'>
+                            <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
                                 <Link href={'/'}>Pricing</Link>
                             </li>
-                            <li className='border-bottom nav-lists'>
+                            <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
                                 <Link href={'/'}>About us</Link>
                             </li>
-                            <li className='border-bottom nav-lists'>
+                            <li className='d-flex justify-content-center align-items-center px-3 px-lg-4'>
                                 <Link href={'/'}>Contact</Link>
-                            </li>
-
-                            
-                            <li className='copyright-container'>
-                                <span className="special">@{BRANDNAME} 2025</span>
                             </li>
                         </ul>
                     </div>
-                </Offcanvas.Body>
-            </Offcanvas>
 
-        
+                    <div className="auth-btns-container d-flex">
+                        <div className="login-btn-container pe-4 pe-lg-4 d-none d-sm-block">
+                            <Link href={'/login'}>
+                                <button className="btn">Log in</button>
+                            </Link>
+                        </div>
+                        <div className="signup-btn-container d-none d-sm-block">
+                            <Link href={'/signup'}>
+                                <button className="btn">Sign up</button>
+                            </Link>
+                        </div>
+                        
+                        <div className="signup-btn-container d-block d-sm-none">
+                            <Link href={'/login'}>
+                                <button className="btn">Log in</button>
+                            </Link>
+                        </div>
 
+                        <div className="launch-offcanvas-btn-container ms-3 ms-sm-4 ms-lg-4 d-flex d-lg-none justify-content-between align-items-center d-lg-none">
+                            <button className="btn d-flex justify-content-between align-items-center"  onClick={handleShow} >
+                                <RxHamburgerMenu />
+                            </button>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="d-none d-lg-block navbar-offcanvas">
+
+                <Offcanvas show={show} onHide={handleClose}>
+                    <Offcanvas.Header className='border-bottom' closeButton>
+                    <Offcanvas.Title>{BRANDNAME}</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <div className="offcanvas-container w-100">
+                            <ul className="ul p-0 m-0">
+                                <li className='border-bottom nav-lists'>
+                                    <Link href={'/'}>Home</Link>
+                                </li>
+                                <li className='border-bottom nav-lists'>
+                                    <Link href={'/'}>Pricing</Link>
+                                </li>
+                                <li className='border-bottom nav-lists'>
+                                    <Link href={'/'}>About us</Link>
+                                </li>
+                                <li className='border-bottom nav-lists'>
+                                    <Link href={'/'}>Contact</Link>
+                                </li>
+
+                                
+                                <li className='copyright-container'>
+                                    <span className="special">@{BRANDNAME} 2025</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </Offcanvas.Body>
+                </Offcanvas>
+
+            
+
+            </div>
         </div>
     </div>
   )
