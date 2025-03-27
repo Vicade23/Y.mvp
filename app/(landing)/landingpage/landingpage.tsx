5c6b184772b1d1a8ffa12912ea5eeea974307dcb
Emmanuel Victor <emavec2002@gmail.com>
2025-03-27 16:01:30 -0700

    creating the landing page

--- app/(landing)/landingpage/landingpage.tsx
+++ app/(landing)/landingpage/landingpage.tsx
@@ -57,7 +57,42 @@ const LandingPage = () => {
             </div>
           </div>
         </section>
+
       </div>
+
+      <div className="container-fluid container-2 p-0">
+
+          <div className="container">
+            
+              <section className="section-3">
+                <div className="card-container row mx-auto">
+
+                  <div className="first-col col-12 col-lg-6">
+                    <div className="sizer">
+
+                      <h2 className="h2">Get started with templates</h2>
+                      <div className="sub-heading">
+                        Bring your presentations to life in Google Slides with enhancements like videos, animations, smooth transitions, and more.
+                      </div>
+                      <div className="btns-containers-container">
+                        <div className="btn-container">
+                          <button className="btn first-btn">Try it for free</button>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+                  <div className="second-col col-12 col-lg-6">
+                    <Image src='/images/join.svg' alt="google icon" width={100} height={100} />
+                  </div>
+                </div>
+              </section>
+
+          </div>
+
+      </div>
+      
+
     </div>
   )
 }
