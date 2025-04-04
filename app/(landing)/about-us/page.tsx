0e5ae4011282e38e50e6ba37eb7962e6489f2cb9
Emmanuel Victor <emavec2002@gmail.com>
2025-04-04 06:59:53 -0700

    completed about page

--- app/(landing)/about-us/page.tsx
+++ app/(landing)/about-us/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './about-us.scss'
+import Image from 'next/image'
+import Getstartedcard from '@/app/components/getstartedcard/getstartedcard'
 
 const AboutUs = () => {
   return (
@@ -26,28 +28,114 @@ const AboutUs = () => {
         </div>
 
       </section>
-        
-        <section className="section-2">
-{/*             
-            <div className="hero">
-              <div className="cover"></div>
-              <div className="img-container"></div>
-            </div> */}
-
-        </section>
 
         <div className="container">
-            
-          <section className="section-2">
+          
+          <section className="section-3">
+  
+            <div className="card-container row mx-auto align-items-center">
+  
+              <div className="col-lg-6 first-col">
+                <div className="sizer">
+                  <h2 className="h2">We can put a funny quote or slogan over here</h2>
+                </div>
+              </div>
+              
+              <div className="second-col col-12 col-lg-6">
+                <Image src='/images/think.svg' alt="google icon" width={200} height={200} />
+              </div>
+  
+            </div>
+  
+          </section>
+
+          
+          <section className="section-4">
 
             <div className="sizer">
-              <h2 className="h2">Our mission</h2>
+              <h2 className="h2">Team</h2>
               <div className="tests-subtitle">
-                Remember to talk about your mission. Why are you building this project? Who you are building this for? Why do you care about solving this problem? Why people should care?
+                Meet the people behind our magical product
               </div>
+            </div>
+            
+            <div className="team row mx-auto">
+
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+              
+              <div className="cols col-6 col-md-3 mb-4">
+                <div className="mx-auto team-header border-danger">
+                  <Image src='/images/avarter.png' alt="google icon" width={200} height={200} />
+                </div>
+                <div className="team-name">Name Name</div>
+                <div className="team-role">Chief-E-Officer</div>
+              </div>
+
             </div>
 
           </section>
+        </div>
+        
+        <div className="container-fluid container-1 p-0">
+            <div className="container">
+                <section className="section-5">
+                  <Getstartedcard />
+                </section>
+            </div>
         </div>
 
 
